56481c68a032910172e0eabd170e06ed56ebb13b
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-10-08 10:30:46 +0900

    add: headers

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -2,6 +2,7 @@ export const handler = async () => {
     console.log('Hello World from Lambda!');
     return {
         statusCode: 200,
+        headers: { "Content-Type": "text/plain" },
         body: JSON.stringify({
             message: 'Hello World from Lambda!',
         }),
