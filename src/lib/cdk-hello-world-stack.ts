c144ac2bf774150f51834201a3be6f316d25b9c9
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-10-10 18:00:54 +0900

    add: IAM role

--- src/lib/cdk-hello-world-stack.ts
+++ src/lib/cdk-hello-world-stack.ts
@@ -2,15 +2,21 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class CdkHelloWorldStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
 
+        const existingRoleArn = 'arn:aws:iam::YOUR_ACCOUNT_ID:role/YOUR_EXISTING_ROLE_NAME';
+        const existingRole = iam.Role.fromRoleArn(this, 'ExistingRole', existingRoleArn);
+
+
         const helloWorldFunction = new lambda.Function(this, 'HelloWorldFunction', {
           runtime: lambda.Runtime.NODEJS_20_X,
           code: lambda.Code.fromAsset('lambda'),
           handler: 'hello.handler',
+          role: existingRole,
         });
 
         const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
