f59a239bfc49344c74c26360dd854deaeae788d3
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-10-16 18:01:50 +0900

    aaa

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,22 @@
+'use strict';
 import * as path from 'path';
 import { Configuration } from 'webpack';
+import * as fs from 'fs';
+// const nodeExternals = require('webpack-node-externals');
+// const CopyPlugin = require('copy-webpack-plugin');
+
+const ROOT_DIR = path.resolve(__dirname);
+// const SRC_DIR = path.join(ROOT_DIR, 'src');
+
+const entry = { ...getEntries('lambda') };
 
 const config: Configuration = {
   // CDK アプリのエントリーポイントを指定
   // Todo　app.tsにかえる
   entry: './bin/hello-world.ts',
   output: {
-    filename: 'hello-world.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    path: path.resolve(ROOT_DIR, '.dist'),
     // CDK アプリは CommonJS モジュールとしてエクスポートされるため
     libraryTarget: 'commonjs2',
   },
@@ -35,3 +44,23 @@ const config: Configuration = {
 };
 
 export default config;
+
+function getEntries(dirname, entries = {}) {
+  const dirpath = path.join(ROOT_DIR, dirname);
+  const dirs = fs.readdirSync(dirpath, { withFileTypes: true });
+  const targets = dirs.filter(({ name }) => name !== 'base');
+
+  for (const dirent of targets) {
+      const { name } = dirent;
+
+      if (dirent.isDirectory()) {
+          Object.assign(entries, getEntries(path.join(dirname, name), entries));
+      }
+      if (dirent.isFile()) {
+          const nameWithoutExtension = name.split('.')[0];
+          entries[`${dirname}/${nameWithoutExtension}/index`] = path.join(dirpath, name);
+      }
+  }
+
+  return entries;
+}
