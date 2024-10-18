0306fa79662371efec2b5f6274072311523b4854
mem-3111-fujiwara <127289111+eg-FujiwaraDaisuke@users.noreply.github.com>
2024-10-18 15:52:32 +0900

    Feature/add vitest (#11)
    * add: vitest package
    
    * add: test command
    
    * fix: test command
    
    * add: hello test code
    
    * add: blank line
    
    * delete: blank line

---
+++ test/lambda/hello.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest';
+import { handler } from '../../lambda/hello';
+
+describe('Lambda Handler', () => {
+    it('should return a valid response', async () => {
+        const response = await handler();
+
+        expect(response.statusCode).toBe(200);
+        expect(response.headers).toEqual({ "Content-Type": "text/plain" });
+        expect(JSON.parse(response.body)).toEqual({
+            message: 'Hello World from Lambda!',
+        });
+    });
+});
