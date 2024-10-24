963542f0b7e9184fbe900c2543b913135ff3d902
mem-3111-fujiwara <127289111+eg-FujiwaraDaisuke@users.noreply.github.com>
2024-10-24 13:01:26 +0900

    スッタクを作るためのcontextを作成 (#16)
    * add: Context
    
    * add: Config
    
    * edit: coment out
    
    * edit: use context
    
    * add: blank line

---
+++ src/config.ts
@@ -0,0 +1,44 @@
+import * as path from 'path';
+
+export class Config {
+    public readonly rootDir = path.join(__dirname, '../');
+    public readonly distDir = path.join(this.rootDir, '.dist');
+
+    /**
+     * 環境別にuserPoolを返す
+     * @param { string } stage - 環境（例: 'dev', 'stg', 'prd'）
+     * @return { string } - userPool文字列
+     */
+    // public userPool(stage: string): string {
+    //     switch (stage) {
+    //         case 'dev':
+    //             return 'ap-northeast-dev';
+    //         case 'stg':
+    //             return 'ap-northeast-stg';
+    //         case 'prd':
+    //             return 'ap-northeast-prd';
+    //         default:
+    //             return '';
+    //     }
+    // }
+
+    /**
+     * 環境別に許可されたオリジンを返す
+     * @param { string } stage - 環境（例: 'dev', 'stg', 'prd'）
+     * @return { string[] } - 許可されたオリジンの配列
+     */
+    // public allowOrigins(stage: string): string[] {
+    //     const localhost = 'http://localhost:3000';
+
+    //     switch (stage) {
+    //         case 'dev':
+    //             return ['https:'];
+    //         case 'stg':
+    //             return ['https:', localhost];
+    //         case 'prd':
+    //             return ['https:'];
+    //         default:
+    //             return [];
+    //     }
+    // }
+}
--- src/lib/cdk-hello-world-stack.ts
+++ src/lib/cdk-hello-world-stack.ts
@@ -4,6 +4,8 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
+import { Context } from '../context';
+import { Config } from '../config';
 
 export class CdkHelloWorldStack extends cdk.Stack {
     constructor(scope: Construct) {
@@ -12,9 +14,40 @@ export class CdkHelloWorldStack extends cdk.Stack {
         const env = { account, region };
         
         const stage: string = scope.node.tryGetContext('stage');
-        const constructId = 'HelloWorld';
-        super(scope, `${stage}-${constructId}`, { env });
+        const stackType = 'cdk-hello-world';
+        const id = `${stage}-${stackType}`;
+        super(scope, id, { env });
 
+        const context: Context = {
+            stage,
+            config: new Config(),
+            stackType: stackType,
+            scope: this,
+            prefix: id,
+        };
+
+        const distDir = context.config.distDir;
+
+        const helloWorldFunction = new lambda.Function(this, 'HelloWorldFunction', {
+          runtime: lambda.Runtime.NODEJS_20_X,
+          code: lambda.Code.fromAsset(
+            path.join(
+              distDir,
+                'lambda'
+            )
+        ),
+          handler: 'hello.handler',
+        });
+
+        const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+          handler: helloWorldFunction,
+          proxy: false,
+        });
+
+        const helloResource = api.root.addResource('hello');
+        helloResource.addMethod('GET');
+
+        // 以下ユーザープールの作成
         // 環境ごとのユーザープール名を取得
         const userPoolName = `${stage}-UserPool`;
 
@@ -35,7 +68,6 @@ export class CdkHelloWorldStack extends cdk.Stack {
             requireDigits: true,
           },
         });
-       
 
         // ユーザープールクライアントの作成
         const userPoolClient = new cognito.UserPoolClient(this, 'MyUserPoolClient', {
@@ -55,27 +87,5 @@ export class CdkHelloWorldStack extends cdk.Stack {
           value: userPoolClient.userPoolClientId,
           description: 'The client ID of the user pool client',
         });
-
-        const rootDir = path.join(__dirname, '../../');
-        const distDir = path.join(rootDir, '.dist');
-
-        const helloWorldFunction = new lambda.Function(this, 'HelloWorldFunction', {
-          runtime: lambda.Runtime.NODEJS_20_X,
-          code: lambda.Code.fromAsset(
-            path.join(
-              distDir,
-                'lambda'
-            )
-        ),
-          handler: 'hello.handler',
-        });
-
-        const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
-          handler: helloWorldFunction,
-          proxy: false,
-        });
-
-        const helloResource = api.root.addResource('hello');
-        helloResource.addMethod('GET');
     }
   }
