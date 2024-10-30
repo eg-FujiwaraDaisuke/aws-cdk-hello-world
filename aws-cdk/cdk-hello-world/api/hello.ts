f5ce94b42cf0dde63cd0e4d3159247843a053ec4
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-10-30 10:43:11 +0900

    add: HelloApi class

---
+++ aws-cdk/cdk-hello-world/api/hello.ts
@@ -0,0 +1,47 @@
+import { Construct } from 'constructs';
+import { Context } from '../../context';
+import { Duration } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
+import { LambdaFunction } from '../../utils/constructs/lambda';
+
+export class HelloApi extends Construct {
+    public constructor(scope: Construct, context: Context) {
+        const constructId = 'HelloApi';
+        super(scope, constructId);
+        const { stackType } = context;
+
+        const apiName = 'hello';
+        const apiPath = 'api';
+
+        const helloWorldFunction = new LambdaFunction(this, {
+            context,
+            lambdaName: apiName,
+            props: {
+                // lambda関数に登録する際に使用するコードまでのディレクトリ
+                code: lambda.Code.fromAsset(
+                    path.join(
+                        context.config.distDir,
+                        `lambda-handler/${apiPath}s/${stackType}/${apiName}`
+                    )
+                ),
+                timeout: Duration.minutes(15),
+            },
+            timeoutDuration: 14 * 60,
+        });
+
+        const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+            handler: helloWorldFunction,
+            proxy: false,
+        });
+
+        // エンドポイントとメソッドを設定
+        // エンドポイント　api/${stackType}/${apiName}
+        const apiResource = api.root.getResource(apiPath) ?? api.root.addResource(apiPath);
+        const stackResource =
+            apiResource.getResource(`${stackType}`) ?? apiResource.addResource(`${stackType}`);
+        const resource = stackResource.getResource(apiName) ?? stackResource.addResource(apiName);
+        resource.addMethod('GET');
+    }
+}
