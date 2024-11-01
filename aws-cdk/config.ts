bf7a141244e291b43554689a743ea56f16e2f7b3
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-11-01 11:12:53 +0900

    fix: Config class

--- aws-cdk/config.ts
+++ aws-cdk/config.ts
@@ -3,23 +3,10 @@ import * as path from 'path';
 export class Config {
     public readonly rootDir = path.join(__dirname, '../');
     public readonly distDir = path.join(this.rootDir, '.dist');
+    public readonly userPoolId: string;
 
-    /**
-     * 環境別にuserPoolIdを返す
-     * @param { string } stage - 環境（例: 'dev', 'stg', 'prd'）
-     * @return { string } - userPoolId文字列
-     */
-    public userPoolId(stage: string): string {
-        switch (stage) {
-            case 'dev':
-                return 'ap-northeast-dev';
-            case 'stg':
-                return 'ap-northeast-stg';
-            case 'prd':
-                return 'ap-northeast-prd';
-            default:
-                return '';
-        }
+    constructor(userPoolId: string) {
+        this.userPoolId = userPoolId;
     }
 
     /**
@@ -27,18 +14,17 @@ export class Config {
      * @param { string } stage - 環境（例: 'dev', 'stg', 'prd'）
      * @return { string[] } - 許可されたオリジンの配列
      */
-    // public allowOrigins(stage: string): string[] {
-    //     const localhost = 'http://localhost:3000';
-
-    //     switch (stage) {
-    //         case 'dev':
-    //             return ['https:'];
-    //         case 'stg':
-    //             return ['https:', localhost];
-    //         case 'prd':
-    //             return ['https:'];
-    //         default:
-    //             return [];
-    //     }
-    // }
+    public allowOrigins(stage: string): string[] {
+        const localhost = 'http://localhost:3000';
+        switch (stage) {
+            case 'dev':
+                return ['https:'];
+            case 'stg':
+                return ['https:', localhost];
+            case 'prd':
+                return ['https:'];
+            default:
+                return [];
+        }
+    }
 }
