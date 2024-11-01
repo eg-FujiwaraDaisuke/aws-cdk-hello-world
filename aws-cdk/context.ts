6f2b01337c00da177d6f1462cd584c68fcb0f939
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-11-01 16:12:06 +0900

    add: Context annotation

--- aws-cdk/context.ts
+++ aws-cdk/context.ts
@@ -1,6 +1,14 @@
 import { Construct } from "constructs";
 import { Config } from "./config";
 
+/**
+ * コンテキスト情報を提供するインターフェース
+ * stage: string  環境(prd・stg・devなど)
+ * config: Config(rootPath・distPath・userPollIdを持つ)
+ * stackType: string  stf03111-${スタック名}
+ * scope: scope
+ * prefix: string  ${stackType}-${stage}
+ */
 export interface Context {
   stage: string;
   config: Config;
