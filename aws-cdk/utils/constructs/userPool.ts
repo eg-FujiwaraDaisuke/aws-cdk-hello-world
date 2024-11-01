9576c9e6a98b519720d3673184996ee4b2463f38
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-11-01 10:36:46 +0900

    add: userPoolId output

--- aws-cdk/utils/constructs/userPool.ts
+++ aws-cdk/utils/constructs/userPool.ts
@@ -1,9 +1,8 @@
+import * as cdk from 'aws-cdk-lib';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 
 export class EnvironmentUserPool extends cognito.UserPool {
-    public userPoolClient: cognito.UserPoolClient;
-
     constructor(scope: Construct, stackId: string) {
         const userPoolName = `${stackId}-userPool`;
 
@@ -25,12 +24,25 @@ export class EnvironmentUserPool extends cognito.UserPool {
             },
         });
 
+        const userPoolClientId = `${stackId}-UserPoolClient`;
+
         // ユーザープールクライアントの作成
-        this.userPoolClient = new cognito.UserPoolClient(scope, `${stackId}-UserPoolClient`, {
+        const userPoolClient = new cognito.UserPoolClient(scope, userPoolClientId, {
             userPool: this,
             authFlows: {
                 userPassword: true,
             },
+        });
+
+        // 出力情報
+        new cdk.CfnOutput(this, 'UserPoolIdOutput', {
+            value: this.userPoolId,
+            description: `The ID of the ${stackId} user pool`,
+        });
+
+        new cdk.CfnOutput(this, 'UserPoolClientIdOutput', {
+            value: userPoolClient.userPoolClientId,
+            description: `The client ID of the ${stackId} user pool client`,
         });
     }
 }
