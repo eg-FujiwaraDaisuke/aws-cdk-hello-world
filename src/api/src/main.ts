2bc6c5387847e9c411daf8931ee9017638555938
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-11-08 14:11:18 +0900

    edit: nest main.ts

--- src/api/src/main.ts
+++ src/api/src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { createServer, proxy } from 'aws-serverless-express';
+import * as express from 'express';
+import { Server } from 'http';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+const binaryMimeTypes: string[] = [];
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    nestApp.setGlobalPrefix('v1');
+    nestApp.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '任意のオリジン')
+      res.header('Access-Control-Allow-Headers', '任意のHTTPヘッダ')
+      res.header(
+        'Access-Control-Allow-Methods',
+        'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'
+      )
+      next()
+    });
+    await nestApp.init();
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+  }
+  return cachedServer;
 }
-bootstrap();
+
+// Lambdaに渡されるhandler
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const handler = async (event: any, context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
