f9c8dd98e8cd799f7158d3163371894f15cdd1c2
Members fujiwara <fujiwara.daisuke.3111@members.co.jp>
2024-11-14 10:24:42 +0900

    add: authenticated iamRole

--- aws-cdk/utils/constructs/userPool.ts
+++ aws-cdk/utils/constructs/userPool.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as iam from "aws-cdk-lib/aws-iam"; // IAM モジュールをインポート
 import { Construct } from "constructs";
 
 export class EnvironmentUserPool extends cognito.UserPool {
@@ -41,11 +42,35 @@ export class EnvironmentUserPool extends cognito.UserPool {
       allowUnauthenticatedIdentities: false,
     });
 
+    // 環境ごとの IAM ロールを作成
+    const authenticatedRole = new iam.Role(scope, `${stackId}-AuthenticatedRole`, {
+      assumedBy: new iam.FederatedPrincipal(
+        'cognito-identity.amazonaws.com',
+        {
+          StringEquals: {
+            'cognito-identity.amazonaws.com:aud': identityPool.ref,
+          },
+          ForAnyValueEquals: {
+            'cognito-identity.amazonaws.com:amr': 'authenticated',
+          },
+        },
+        'sts:AssumeRoleWithWebIdentity',
+      ),
+    });
+
+    const lambdaAccessPolicy = new iam.PolicyStatement({
+      actions: ['lambda:InvokeFunction'],
+      resources: [`arn:aws:lambda:${cdk.Stack.of(scope).region}:${cdk.Stack.of(scope).account}:function:${stackId}-*`], // 環境ごとの Lambda 関数にアクセス
+    });
+
+    // IAM ロールにポリシーを追加
+    authenticatedRole.addToPolicy(lambdaAccessPolicy);
+
     // Cognito Identity PoolとCognito User Poolの関連付け
     new cognito.CfnIdentityPoolRoleAttachment(scope, `${stackId}-identityPoolRoleAttachment`, {
       identityPoolId: identityPool.ref,
       roles: {
-        authenticated: '',
+        authenticated: authenticatedRole.roleArn, // IAM ロールの ARN を指定
       },
     });
 
